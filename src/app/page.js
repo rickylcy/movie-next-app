25cc15b6f280c2539d8b1924a0181703f4d9cc1c
Ricky Lau <lauyin8183@gmail.com>
2024-09-17 16:47:01 +1000

    shadcn added

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <div className="flex gap-6 py-6">
+        <Button>Learn More</Button>
+      </div>
+    </div>
+  );
 }
